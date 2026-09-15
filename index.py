7e5a760a0e2b9d0bdbca13f2a3cf429b4b2d2784
Shreshta <117246220+Shresht5@users.noreply.github.com>
2026-09-15 14:11:16 +0530

    integration of huggingface api models

--- index.py
+++ index.py
@@ -2,13 +2,20 @@ import langchain
 from fastapi import FastAPI
 from typing import Union
 from dotenv import load_dotenv
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 
 load_dotenv()
 
 from langchain.chat_models import init_chat_model
 
-model = init_chat_model("open-mistral-7b", model_provider="mistralai")
-res=model.invoke("what is cricket?")
+# model = init_chat_model("open-mistral-7b", model_provider="mistralai" ,temperature=0.6,max_tokens=20)
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-R1",
+    temperature=0.7,
+    max_new_tokens=100,
+)
+model = ChatHuggingFace(llm=llm)
+res=model.invoke("what is cricket?" )
 # print(model)
 print(res)
 
